184e288e68c1f423b32f6f07617efddf2896e4b4
DeadHello <79786672+s1072489@users.noreply.github.com>
2024-08-02 21:10:33 +1000

    Bugfix + Logging
    - Fixed using camera 1 when Raspberry Pi USB camera is camera 0
    - Added logging to raspi-cam

--- src/raspi-camera.py
+++ src/raspi-camera.py
@@ -1,32 +1,43 @@
 # Simple script to record 10min long videos from a Raspberry Pi
 
-import cv2 as cv
 import time
+import logging
+import cv2 as cv
 from datetime import datetime
 
 if __name__ == "__main__":
-    capture = cv.VideoCapture(1)
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO, filename="raspi-camera.log", format="%(asctime)s - %(levelname)s - %(message)s")
 
-    capture.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
-    capture.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
-    capture.set(cv.CAP_PROP_FPS, 30)
+    try:
+        capture = cv.VideoCapture(0)
 
-    while True:
-        start_time = time.time()
+        capture.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
+        capture.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
+        capture.set(cv.CAP_PROP_FPS, 30)
 
-        result = cv.VideoWriter(f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}.mp4", cv.VideoWriter_fourcc(*'avc1'), 30, (1280, 720))
+        logger.info("Camera initialized")
 
         while True:
-            ret, frame = capture.read()
+            start_time = time.time()
+            result = cv.VideoWriter(f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}.mp4", cv.VideoWriter_fourcc(*'avc1'), 30, (1280, 720))
+            logger.info("Recording started")
 
-            if not ret:
-                break
+            while True:
+                ret, frame = capture.read()
 
-            if cv.waitKey(1) & 0xFF == ord('s'):
-                break
+                if not ret:
+                    break
 
-            result.write(frame)
+                if cv.waitKey(1) & 0xFF == ord('s'):
+                    break
 
-            if time.time() - start_time >= 600:
-                result.release()
-                break
+                result.write(frame)
+
+                if time.time() - start_time >= 600:
+                    result.release()
+                    logger.info("Recording completed")
+                    break
+
+    except Exception as e:
+        logger.error(e, exc_info=True)
